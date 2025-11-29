ebe3c4073ca8d32673d9ec91baa2c1a251b1fb28
Jim Shaw <jimxshaw@gmail.com>
2025-11-29 11:44:15 -0600

    Update Flux.Forward program.cs.

--- Flux.Forwarder/Program.cs
+++ Flux.Forwarder/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+
+var targetHost = "flux-service";   // Docker Compose service name = DNS name.
+var targetPort = 5140;
+var interval = TimeSpan.FromSeconds(2);
+
+using var udpClient = new UdpClient();
+
+Console.WriteLine($"[Forwarder] Sending logs to {targetHost}:{targetPort} every {interval.TotalSeconds} seconds...");
+
+while (true)
+{
+  var message = $"Fake log from Forwarder at {DateTime.UtcNow:O}";
+  var bytes = Encoding.UTF8.GetBytes(message);
+
+  try
+  {
+    await udpClient.SendAsync(bytes, bytes.Length, targetHost, targetPort);
+    Console.WriteLine($"[Forwarder] Sent: {message}");
+  }
+  catch (Exception ex)
+  {
+    Console.WriteLine($"[Forwarder] Error: {ex.Message}");
+  }
+
+  await Task.Delay(interval);
+}
